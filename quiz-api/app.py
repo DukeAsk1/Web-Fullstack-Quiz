b3797d3731f607f0ff3dd6e8ab4394eb857b4647
Hoang-Duc DUONG <hoang-duc.duong@edu.esiee.fr>
2022-12-13 16:25:19 +0100

    add login part 4 and debut docker

--- quiz-api/app.py
+++ quiz-api/app.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
+from jwt_utils import build_token,decode_token,secret
 
 app = Flask(__name__)
 CORS(app)
@@ -12,6 +13,16 @@ def hello_world():
 @app.route('/quiz-info', methods=['GET'])
 def GetQuizInfo():
 	return {"size": 0, "scores": []}, 200
+
+@app.route('/login', methods=['POST'])
+def login():
+    payload = request.get_json()
+    if payload.get('password') == secret :
+        return  {"token" : build_token()} , 200
+
+    else:
+        return "Unauthorized", 401
+
 
 if __name__ == "__main__":
     app.run()
