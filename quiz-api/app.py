0709f40e4ae73169dd8fe9dc4238c08db6238014
Hoang-Duc DUONG <hoang-duc.duong@edu.esiee.fr>
2022-12-13 19:27:25 +0100

    debut partie 5

--- quiz-api/app.py
+++ quiz-api/app.py
@@ -1,9 +1,25 @@
 from flask import Flask, request
 from flask_cors import CORS
 from jwt_utils import build_token,decode_token,secret
+import sqlite3
 
 app = Flask(__name__)
 CORS(app)
+
+file='tuto.db'
+
+def connect_to_db(path):
+    db_connection = sqlite3.connect(path)
+
+    # set the sqlite connection in "manual transaction mode"
+    # (by default, all execute calls are performed in their own transactions, not what we want)
+    db_connection.isolation_level = None
+    return db_connection
+
+# Exemple de création de classe en python
+class Question():
+	def init(self, title: str):
+		self.title = title
 
 @app.route('/')
 def hello_world():
@@ -22,6 +38,31 @@ def login():
 
     else:
         return "Unauthorized", 401
+
+@app.route('/add_questions',methods=['POST'])
+def question():
+    cur = connect_to_db(file)
+    #Récupérer le token envoyé en paramètre
+    request.headers.get('Authorization')
+
+    #récupèrer un l'objet json envoyé dans le body de la requète
+    payload = request.get_json()
+
+    # start transaction
+    cur.execute("begin")
+
+    # save the question to db
+    cur.execute(
+        f"insert into Question (Title) values"
+        f"('{payload.title}')")
+
+    # send the request
+    cur.execute("commit")
+
+    # in case of exception, rollback the transaction
+    cur.execute('rollback')
+    return payload
+
 
 
 if __name__ == "__main__":
