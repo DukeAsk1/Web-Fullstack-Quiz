f232c7e75fac41fb0ecd589e5cd755242ab0f464
Hoang-Duc DUONG <dduong1804@yahoo.fr>
2022-12-29 11:09:32 +0100

    update manager to display

--- quiz-ui/src/views/QuestionDisplay.vue
+++ quiz-ui/src/views/QuestionDisplay.vue
@@ -1,7 +1,23 @@
 <template>
   <div class="question-display">
     <h1>Question Display</h1>
-    <img v-if="question.image" :src="question.image" /> <a @click="$emit('answer-selected', 2)">La réponse D</a>
+
+    <h1>{{ question.title }}</h1>
+    <h1>{{ question.text }}</h1>
+
+    <img v-if="question.image" :src="question.image" />
+    <!-- <div>
+      Answer Selected : {{ selected.text }}
+      Scores : {{ score }}
+    </div> -->
+
+    <!-- <div v-for="value in currentQuestion.possibleAnswers" :key="value">
+      <input type="radio" v-model="selected" :value="value" /> {{ value.text }}
+    </div> -->
+    <a v-for="(answer, index) in question.possibleAnswers" @click="$emit('answer-selected', index)">
+      {{ answer.text }}
+    </a>
+
   </div>
 </template>
 
@@ -14,9 +30,9 @@ export default {
       type: Object
     },
     emits: ["answer-selected"],
-    created() {
-      this.$emit('answer-selected', 2);
-    },
+    // created() {
+    //   this.$emit('answer-selected', 2);
+    // },
   }
 };
 </script>
--- quiz-ui/src/views/QuestionsManager.vue
+++ quiz-ui/src/views/QuestionsManager.vue
@@ -2,23 +2,12 @@
   <div class="questions-manager">
     <h1>Questions Manager</h1>
     <h1>Question {{ currentQuestionPosition }} / {{ totalNumberOfQuestion }}</h1>
-    <h1>{{ currentQuestion.questionTitle }}</h1>
-    <h1>{{ currentQuestion.questionText }}</h1>
-    <div>
-      Answer Selected : {{ selected.text }}
-      Scores : {{ score }}
 
-    </div>
-
-
-
-    <div v-for="value in currentQuestion.possibleAnswers" :key="value">
-      <input type="radio" v-model="selected" :value="value" /> {{ value.text }}
-    </div>
-    <!-- <QuestionDisplay :question="currentQuestion" @click-on-answer="answerClickedHandler" /> -->
+    <QuestionDisplay :question="currentQuestion" @click-on-answer="answerClickedHandler" />
   </div>
 
-  <button @click="next_question">Next</button>
+  <button v-if="currentQuestionPosition != totalNumberOfQuestion" @click="next_question">Next</button>
+
 
 
 
@@ -56,25 +45,61 @@ export default {
   components: {
     QuestionDisplay
   },
+  mounted() {
+    // const answer_list = sessionStorage.getItem('list_of_answers');
+    // const ongoing_score = sessionStorage.getItem('score');
+    // const position = sessionStorage.getItem('position');
+
+    // console.log(answer_list);
+
+    // this.list_of_answers = answer_list;
+    // this.score = ongoing_score;
+    // this.currentQuestionPosition = position;
+
+    //   try {
+    //     // Something that throws exception
+    //     const answer_list = sessionStorage.getItem('list_of_answers');
+    //     this.list_of_answers = answer_list;
+    //   }
+    //   catch (e) {
+    //     this.list_of_answers = [];
+    //   }
+    try {
+      // Something that throws exception
+      const ongoing_score = sessionStorage.getItem('score');
+      this.score = ongoing_score;
+      console.log(this.score);
+    }
+    catch (e) {
+      this.score = 0;
+    }
+    //   try {
+    //     // Something that throws exception
+    //     const position = sessionStorage.getItem('position');
+    //     this.currentQuestionPosition = position;
+    //   }
+    //   catch (e) {
+    //     this.currentQuestionPosition = 1;
+    //   }
+  },
   async created() {
-    let currentQuestion = await this.loadQuestionByposition();
-    this.currentQuestion.questionTitle = currentQuestion.data.title;
-    this.currentQuestion.questionText = currentQuestion.data.text;
-    this.currentQuestion.possibleAnswers = currentQuestion.data.possibleAnswers;
+    this.currentQuestion = await this.loadQuestionByposition(this.currentQuestionPosition);
+    // this.currentQuestion.questionTitle = currentQuestion.data.title;
+    // this.currentQuestion.questionText = currentQuestion.data.text;
+    // this.currentQuestion.possibleAnswers = currentQuestion.data.possibleAnswers;
 
     let nb_question = await this.getNumberOfQuestion();
     this.totalNumberOfQuestion = nb_question.data.nb_question;
     // this.value = this.value;
     // console.log(this.selected.isCorrect);
     //this.score = this.calculateScore(value);
-    await this.getCorrectAnswer();
-
-
-
+    //await this.getCorrectAnswer();
   },
   methods: {
-    loadQuestionByposition() {
-      return quizApiService.getQuestion(this.currentQuestionPosition)
+    async loadQuestionByposition(currentQuestionPosition) {
+      let current_question = quizApiService.getQuestion(currentQuestionPosition);
+      let data = await current_question.data;
+      return data
     },
     getCorrectAnswer() {
       for (let i = 0; i <= this.currentQuestion.possibleAnswers.length - 1; i++) {
@@ -82,10 +107,6 @@ export default {
           this.answer_current_question.correct = this.currentQuestion.possibleAnswers[i].text;
         }
       }
-      // for (value in this.currentQuestion.possibleAnswers) {
-
-      // }
-      console.log(this.answer_current_question.correct);
     },
     getNumberOfQuestion() {
       return quizApiService.getNumberOfQuestion()
@@ -99,42 +120,67 @@ export default {
       //   }
       // }
       if (this.selected.isCorrect === true) {
-        this.score = 1;
+        this.score++;
       }
       else {
-        this.score = 0;
+        this.score;
       }
     },
     next_question() {
+      const selectedAnswer = this.selected;
+      // const answer = this.currentQuestion.flatMap(currentQuestion => 
+      //                                     currentQuestion.possibleAnswers).find(possibleAnswers => 
+      //                                     possibleAnswers.id === selectedAnswer);
+      if (selectedAnswer.isCorrect) {
+        this.score++;
+      }
+
       this.answer_current_question.index = this.currentQuestionPosition;
       this.answer_current_question.answer = this.selected.text;
       this.getCorrectAnswer();
       this.list_of_answers.push(this.answer_current_question);
+      this.currentQuestionPosition++;
+      //this.calculateScore();
+      console.log('new position');
+      console.log(this.currentQuestionPosition);
+      this.currentQuestion = this.loadQuestionByposition(this.currentQuestionPosition);
+      console.log(this.currentQuestion);
 
-      console.log(this.list_of_answers);
+      this.currentQuestion.questionTitle = currentQuestion.data.title;
+      this.currentQuestion.questionText = currentQuestion.data.text;
+      this.currentQuestion.possibleAnswers = currentQuestion.data.possibleAnswers;
+      // sessionStorage.setItem('list_of_answers', this.list_of_answers);
+      // sessionStorage.setItem('score', this.score);
+      // sessionStorage.setItem('position', this.currentQuestionPosition);
+      // console.log(this.list_of_answers);
+      // console.log(this.answer_current_question);
+      // console.log(sessionStorage);
+
+      // location.reload();
     },
     answerClickedHandler() {
     },
     endQuiz() {
     },
   },
-  watch: {
-    // watch for changes to the selected property
-    selected: {
-      handler() {
-        try {
-          for (answer in this.currentQuestion.possibleAnswers) {
-            console.log(answer);
-          }
-        }
-        catch (e) { }
-        // console.log(this.currentQuestion.possibleAnswers);
+  // watch: {
+  //   // watch for changes to the selected property
+  //   selected: {
+  //     handler() {
+  //       console.log(this.list_of_answers)
+  //       try {
+  //         for (answer in this.currentQuestion.possibleAnswers) {
+  //           console.log(answer);
+  //         }
+  //       }
+  //       catch (e) { }
+  //       // console.log(this.currentQuestion.possibleAnswers);
 
-        this.calculateScore()
-      },
-      immediate: true,
-    },
-  },
+  //       this.calculateScore();
+  //     },
+  //     immediate: true,
+  //   },
+  // },
   /*computed: {
     totalNumberOfQuestion: function () {
       console.log(Object.values(this.getNumberOfQuestion()))
